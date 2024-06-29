f0295befa2b3db15a74212ab754e37136b529786
sudoer <hhj@sudoer.cn>
2024-06-29 22:08:07 +0800

    初始化项目

---
+++ src/test/java/cn/sudoer/javaproj/JavaprojApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.sudoer.javaproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
