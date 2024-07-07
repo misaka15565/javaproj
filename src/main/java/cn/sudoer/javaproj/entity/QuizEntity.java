7dccad36e36856566e666ddcc566cf8e0081d9c6
sudoer <hhj@sudoer.cn>
2024-07-07 23:03:38 +0800

    改成只有两个数

--- src/main/java/cn/sudoer/javaproj/entity/QuizEntity.java
+++ src/main/java/cn/sudoer/javaproj/entity/QuizEntity.java
@@ -5,7 +5,6 @@ import static cn.sudoer.javaproj.entity.QuizType.*;
 import org.slf4j.LoggerFactory;
 
 import static cn.sudoer.javaproj.entity.Operator.*;
-import static cn.sudoer.javaproj.entity.QuizFormat.*;
 
 //单个数学题
 //鉴于是小学生的，答案和题目都是整数
@@ -15,11 +14,44 @@ public class QuizEntity {
     private QuizType type;
     private Integer num1;
     private Integer num2;
-    private Integer num3;
     private Integer answer;
-    private Operator operator1;// 三个数的题目才会用到，因为两个数的题目只有一个运算符，可以从type里面推断
-    private Operator operator2;
-    private QuizFormat format;// 括号状态（也是三个数的题目才会用到
+    private Operator operator;
+    public QuizType getType() {
+        return type;
+    }
+    public void setType(QuizType type) {
+        this.type = type;
+    }
+    public Integer getNum1() {
+        return num1;
+    }
+    public void setNum1(Integer num1) {
+        this.num1 = num1;
+    }
+    public Integer getNum2() {
+        return num2;
+    }
+    public void setNum2(Integer num2) {
+        this.num2 = num2;
+    }
+    public Integer getAnswer() {
+        return answer;
+    }
+    public void setAnswer(Integer answer) {
+        this.answer = answer;
+    }
+    public Operator getOperator() {
+        return operator;
+    }
+    public void setOperator(Operator operator1) {
+        this.operator = operator1;
+    }
+    public String Quiz_toString() {
+        return num1 + operatorStrings[operator.ordinal()] + num2;
+    }
+    public String Quiz_toStringWithAnswer() {
+        return num1 + operatorStrings[operator.ordinal()] + num2 + "=" + answer;
+    }
 
     private final static String[] operatorStrings = { "+", "-", "×", "÷" };// 注意编码问题，÷是除号，不是斜杠
 
@@ -38,82 +70,35 @@ public class QuizEntity {
                 return 0;
         }
     }
-
-    private void generateMixedQuiz(Integer numOfDigits) {
-        this.num1 = (int) (Math.random() * Math.pow(10, numOfDigits));
-        this.num2 = (int) (Math.random() * Math.pow(10, numOfDigits));
-        this.num3 = (int) (Math.random() * Math.pow(10, numOfDigits));
-        this.operator1 = Operator.values()[(int) (Math.random() * 4)];
-        this.operator2 = Operator.values()[(int) (Math.random() * 4)];
-        this.format = QuizFormat.values()[(int) (Math.random() * 3)];
-
-        // 检查括号必要性，当题目中同时有低级运算符和高级运算符时，加在低级运算符上的括号才是需要的
-        int[] operatorPriority = { 0, 0, 1, 1 };
-        if (this.format == QuizFormat_BRACKET_1_2) {
-            if (operatorPriority[this.operator1.ordinal()] < operatorPriority[this.operator2.ordinal()]) {
-                this.format = QuizFormat_NO_BRACKET;
-            }
-        }
-        if (this.format == QuizFormat_BRACKET_2_3) {
-            if (operatorPriority[this.operator2.ordinal()] < operatorPriority[this.operator1.ordinal()]) {
-                this.format = QuizFormat_NO_BRACKET;
-            }
-        }
-        // 计算答案
-        if (this.format == QuizFormat_NO_BRACKET) {
-            // 比较两个运算符的优先级
-            if (operatorPriority[this.operator1.ordinal()] < operatorPriority[this.operator2.ordinal()]) {
-                // 第一个运算符优先级低，先算后两个数
-                this.answer = calcualte(this.num2, this.num3, this.operator2);
-                // 再算第一个数
-                this.answer = calcualte(this.num1, this.answer, this.operator1);
-            }else{
-                // 第二个运算符优先级低或者运算符优先级相同，先算前两个数
-                this.answer = calcualte(this.num1, this.num2, this.operator1);
-                // 再算第三个数
-                this.answer = calcualte(this.answer, this.num3, this.operator2);
-            }
-        }else if(this.format==QuizFormat_BRACKET_1_2){
-            // 先算第一个数和第二个数
-            int temp = calcualte(this.num1, this.num2, this.operator1);
-            // 再算第三个数
-            this.answer = calcualte(temp, this.num3, this.operator2);
-        }else{
-            // 先算第二个数和第三个数
-            int temp = calcualte(this.num2, this.num3, this.operator2);
-            // 再算第一个数
-            this.answer = calcualte(this.num1, temp, this.operator1);
-        }
-    }
-
     // 题目类型、位数
     // 题目中三个数和答案最多有多少位，至少有一个达到这个位数
     public QuizEntity(QuizType type, Integer numOfDigits) {
         this.type = type;
-        if (this.type != QuizType_MIX) {
-            this.operator1 = Operator.values()[(int) (Math.random() * 4)];
-            this.num1 = (int) (Math.random() * Math.pow(10, numOfDigits));
-            this.num2 = (int) (Math.random() * Math.pow(10, numOfDigits));
-            this.answer = this.calcualte(numOfDigits, numOfDigits, this.operator1);
-            if(this.type==QuizType_SUB){
-                // 减法的结果不能为负数
-                while(this.answer<0){
-                    this.num1 = (int) (Math.random() * Math.pow(10, numOfDigits));
-                    this.num2 = (int) (Math.random() * Math.pow(10, numOfDigits));
-                    this.answer = this.calcualte(numOfDigits, numOfDigits, this.operator1);
-                }
-            }else if(this.type==QuizType_DIV){
-                // 除法的结果不能为小数
-                while(this.num1%this.num2!=0){
-                    this.num1 = (int) (Math.random() * Math.pow(10, numOfDigits));
-                    this.num2 = (int) (Math.random() * Math.pow(10, numOfDigits));
-                    this.answer = this.calcualte(numOfDigits, numOfDigits, this.operator1);
-                }
+        if (this.type == QuizType_MIX) {
+            // 随机生成一个类型
+            this.type = QuizType.values()[(int) (Math.random() * 4)];
+        }
+        this.operator = Operator.values()[this.type.ordinal()];
+        this.num1 = (int) (Math.random() * Math.pow(10, numOfDigits));
+        this.num2 = (int) (Math.random() * Math.pow(10, numOfDigits));
+        this.answer = this.calcualte(numOfDigits, numOfDigits, this.operator);
+        if (this.type == QuizType_SUB) {
+            // 减法的结果不能为负数
+            while (this.answer < 0) {
+                this.num1 = (int) (Math.random() * Math.pow(10, numOfDigits));
+                this.num2 = (int) (Math.random() * Math.pow(10, numOfDigits));
+                this.answer = this.calcualte(numOfDigits, numOfDigits, this.operator);
             }
-        } else {
-            this.generateMixedQuiz(numOfDigits);
+        } else if (this.type == QuizType_DIV) {
+            // 除法的结果不能为小数
+            while (this.num1 % this.num2 != 0) {
+                this.num1 = (int) (Math.random() * Math.pow(10, numOfDigits));
+                this.num2 = (int) (Math.random() * Math.pow(10, numOfDigits));
+                this.answer = this.calcualte(numOfDigits, numOfDigits, this.operator);
+            }
         }
     }
+    
 }
 
 enum QuizType {
@@ -122,9 +107,4 @@ enum QuizType {
 
 enum Operator {
     Operator_ADD, Operator_SUB, Operator_MUL, Operator_DIV
-}
-
-// 无括号、括号在第一第二个数、括号在第二第三个数
-enum QuizFormat {
-    QuizFormat_NO_BRACKET, QuizFormat_BRACKET_1_2, QuizFormat_BRACKET_2_3
 }
